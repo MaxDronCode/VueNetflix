f7d81db29e6eaf1a4d91efa8e572a82aea993ee4
Max Dron <maxdron@icloud.com>
2024-05-09 20:22:09 +0200

    Footer Responsive

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="div1">
-            <img :src=logoUrl alt="descripcion">
+            <img :src="logoUrl" alt="Logo MaxFlix">
             <button>ÚNETE A LA COMUNIDAD</button>
         </div>
         <div class="div-general">
@@ -21,14 +21,14 @@
             </div>
             <div class="div2">
                 <p>COMUNIDAD</p>
-                <a href="">Directrices</a><br/>
-                <a href="">Discusiones</a><br/>
-                <a href="">Tabla de Clasificación</a><br/>
+                <a href="">Directrices</a><br />
+                <a href="">Discusiones</a><br />
+                <a href="">Tabla de Clasificación</a><br />
             </div>
             <div class="div2">
                 <p>LEGAL</p>
-                <a href="">Términos de uso</a><br/>
-                <a href="">Términos de uso de la API</a><br/>
+                <a href="">Términos de uso</a><br />
+                <a href="">Términos de uso de la API</a><br />
                 <a href="">Política de Privacidad</a>
             </div>
         </div>
@@ -37,71 +37,145 @@
 
 <script>
 import logoMaxFlix from '@/assets/logoMaxFlix.webp';
+
 export default {
     name: 'FooterComponent',
-    data(){
+    data() {
         return {
             logoUrl: logoMaxFlix
         }
     }
 }
-
 </script>
 
 <style scoped>
-*{
-    /* border: 1px solid white; */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
-.div-general{
-    display: flex;
-    justify-content: space-evenly;
-    margin-top: 60px;
-    /* margin: auto; */
-    gap: 50px;
 
-}
-.container{
+.container {
     display: flex;
-    background-color: #273644;
-    height: 320px;
-    width: 100%;
+    flex-direction: row;
     justify-content: space-evenly;
+    align-items: center;
+    background-color: #273644;
+    padding: 20px;
+    gap: 30px;
+    flex-wrap: wrap;
 }
-.div1{
+
+.div1 {
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
+    align-items: center;
     padding: 20px;
-    
+    text-align: center;
+    gap: 15px;
 }
-img{
-    width: 200px;
-    height: auto;
-    border-radius: 100%;
-    margin: auto;
-    
+
+img {
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
 }
-button{
+
+button {
     background-color: #f9ebda;
     color: #273644;
-    margin: auto;
-    width: 300px;
+    width: 250px;
     height: 40px;
     border-radius: 6px;
     font-size: 16px;
     font-weight: 500;
+    cursor: pointer;
+    transition: background-color 0.3s;
 }
-.div2{
+
+button:hover {
+    background-color: #e5dbcb;
+}
+
+.div-general {
+    display: flex;
+    justify-content: space-evenly;
+    gap: 50px;
+    flex-wrap: wrap;
     text-align: left;
-    
+    max-width: 800px;
 }
-a{
-    text-decoration: none;
-    color: #f9ebda;
-}
-p{
+
+.div2 p {
     font-weight: 700;
     font-size: 20px;
     color: whitesmoke;
+    margin-bottom: 10px;
+}
+
+.div2 a {
+    text-decoration: none;
+    color: #f9ebda;
+    margin-bottom: 5px;
+    display: block;
+}
+
+.div2 a:hover {
+    text-decoration: underline;
+}
+
+@media (max-width: 768px) {
+    .container {
+        flex-direction: column;
+        /* align-items: flex-start; */
+        gap: 20px;
+    }
+
+    .div1 {
+        align-items: flex-start;
+        text-align: left;
+    }
+
+    /* img {
+        width: 120px;
+        height: 120px;
+    } */
+
+    button {
+        width: 200px;
+    }
+
+    .div-general {
+        /* gap: 30px; */
+        max-width: 100%;
+    }
+
+    .div2 p {
+        font-size: 18px;
+    }
+
+    .div2 a {
+        font-size: 14px;
+    }
+}
+
+@media (max-width: 480px) {
+    /* img {
+        width: 100px;
+        height: 100px;
+    } */
+
+    button {
+        width: 180px;
+        font-size: 14px;
+    }
+
+    .div2 p {
+        font-size: 16px;
+    }
+
+    .div2 a {
+        font-size: 12px;
+    }
 }
 </style>
